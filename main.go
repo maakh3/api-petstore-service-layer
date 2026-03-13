4545792f660319522f5ee43007e389c74061a40a
Maakhe Ndhlela <maakhe.ndhlela@sainsburys.co.uk>
2026-03-13 02:49:09 +0000

    all pet and store endpoints with placeholders for handlers

--- main.go
+++ main.go
@@ -6,8 +6,22 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, World!")
-	})
+
+	// pet endpoints
+	http.HandleFunc("PUT /pet", _)
+	http.HandleFunc("POST /pet", _)
+	http.HandleFunc("GET /pet/findByStatus", _)
+	http.HandleFunc("GET /pet/findByTags", _)
+	http.HandleFunc("GET /pet/{petId}", _)
+	http.HandleFunc("POST /pet/{petId}", _)
+	http.HandleFunc("DELETE /pet/{petId}", _)
+	http.HandleFunc("POST /pet/{petId}/uploadImage", _)
+
+	// store endpoints
+	http.HandleFunc("GET /store/inventory", _)
+	http.HandleFunc("POST /store/order", _)
+	http.HandleFunc("GET /store/order/{storeId}", _)
+	http.HandleFunc("DELETE /store/order/{storeId}", _)
+
 	http.ListenAndServe(":8080", nil)
 }
